13c9af4b864b7a77cbdfbf5e79499ddc6cb624ab
Christophe Vichery <vichery.christophe@gmail.com>
2020-11-04 16:12:49 -0800

    Chapter 1 - Title 1 - Section 1

---
+++ SwiftUI-Training/SwiftUI-Training/SwiftUI_TrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TrainingApp.swift
+//  SwiftUI-Training
+//
+//  Created by ToF on 11/2/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
